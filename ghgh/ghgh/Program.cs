9a0287f7d55262006e5fe895c376500c1aecc332
svetasimahina5-spec <svetasimahina5@gmail.com>
2026-09-12 10:59:28 +0300

    прописали ввод натурального числа

---
+++ ghgh/ghgh/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ghgh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = NaturalNumber();
+
+            int sum = SlozSum(number);
+
+            Console.WriteLine($"Сумма цифр числа:{number} равна:{sum}");
+        }
+        static int NaturalNumber()
+        {
+            int number;
+            while (true)
+            {
+                Console.Write("Введите натуральное число: ");
+                string input = Console.ReadLine();
+
+                if (int.TryParse(input, out number)&& number >0 )
+                {
+                    return number;
+                }
+
+                Console.WriteLine("Ошибка. Введите корректное натуральное число (больше 0)");
+            }
+        }
+    }
+}
