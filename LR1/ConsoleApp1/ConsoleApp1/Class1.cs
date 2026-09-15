87ba2fdc6e1782462d5bddd182f2168033ae901a
svetasimahina5-spec <svetasimahina5@gmail.com>
2026-09-15 18:29:47 +0300

    Add files via upload

---
+++ LR1/ConsoleApp1/ConsoleApp1/Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Class1
+    {
+        public static double ReadPositiveDouble(string prompt)
+        {
+            double value;
+            while (true)
+            {
+                Console.Write(prompt);
+                string input = Console.ReadLine();
+
+                if (double.TryParse(input, out value) && value > 0)
+                    return value;
+
+                Console.WriteLine("Ошибка: введите положительное число. Повторите ввод.");
+            }
+        }
+
+        public static int CalculateStrips(double length, double width, double rollWidth)
+        {
+            double perimeter = 2 * (length + width);
+            return (int)Math.Ceiling(perimeter / rollWidth);
+        }
+
+        public static int CalculateStripsPerRoll(double rollLength, double height)
+        {
+            double stripLength = height + 0.1;
+            return (int)(rollLength / stripLength);
+        }
+
+        public static int CalculateRolls(int totalStrips, int stripsPerRoll)
+        {
+            return (int)Math.Ceiling((double)totalStrips / stripsPerRoll);
+        }
+    }
+}
---
+++ LR1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double length = Class1.ReadPositiveDouble("Введите длину комнаты (м): ");
+            double width = Class1.ReadPositiveDouble("Введите ширину комнаты (м): ");
+            double height = Class1.ReadPositiveDouble("Введите высоту комнаты (м): ");
+            double rollWidth = Class1.ReadPositiveDouble("Введите ширину рулона (м): ");
+            double rollLength = Class1.ReadPositiveDouble("Введите длину рулона (м): ");
+
+            int totalStrips = Class1.CalculateStrips(length, width, rollWidth);
+            int stripsPerRoll = Class1.CalculateStripsPerRoll(rollLength, height);
+            int rolls = Class1.CalculateRolls(totalStrips, stripsPerRoll);
+
+            Console.WriteLine("Количество полос: " + totalStrips);
+            Console.WriteLine("Количество рулонов: " + rolls);
+        }
+    }
+}
